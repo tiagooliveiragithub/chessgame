1cf76f0dae6a47e0ce09c89a4fd935a24fb54f73
tiagooliveiragithub <contacto.tiagooliveira@gmail.com>
2023-06-29 00:53:29 +0100

    Chess Layer and printing the board

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import Chess.ChessMatch;
 import boardgame.Board;
-import boardgame.Position;
 
 public class Main {
     public static void main(String[] args) {
 
-        Board board = new Board(8,8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
 
     }
 }
