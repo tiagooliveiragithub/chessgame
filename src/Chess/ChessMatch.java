92feff52dd07999324400651a46e775cadcc9c92
tiagooliveiragithub <contacto.tiagooliveira@gmail.com>
2023-06-29 02:16:16 +0100

    ChessException and ChessPosition

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -3,7 +3,6 @@ package Chess;
 import Chess.pieces.King;
 import Chess.pieces.Rook;
 import boardgame.Board;
-import boardgame.Position;
 
 public class ChessMatch {
 
@@ -24,10 +23,13 @@ public class ChessMatch {
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
     private void initialSetup() {
-        board.placePiece(new King(board, Color.WHITE), new Position(3,1));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(5,1));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(5,1));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import Chess.ChessMatch;
-import boardgame.Board;
-
 public class Main {
     public static void main(String[] args) {
 
