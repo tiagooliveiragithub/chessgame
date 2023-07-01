f67f14caff32be35667454d0369c4d8b1256e5af
tiagooliveiragithub <contacto.tiagooliveira@gmail.com>
2023-07-01 02:53:24 +0100

    Bishop

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,5 +1,6 @@
 package Chess;
 
+import Chess.pieces.Bishop;
 import Chess.pieces.King;
 import Chess.pieces.Pawn;
 import Chess.pieces.Rook;
@@ -183,6 +184,8 @@ public class ChessMatch {
         piecesOnTheBoard.add(piece);
     }
     private void initialSetup() {
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -195,6 +198,9 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
